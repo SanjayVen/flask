d0943a108f74eaec9906056d73eca0ce3cfd494a
hippopotamusfoxnotes <145168951+hippopotamusfoxnotes@users.noreply.github.com>
2023-10-27 19:17:17 -0500

    fixed repo

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        return redirect(url_for('index'))
+
+    return render_template('login.html')

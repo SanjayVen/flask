0b219604d62c30970f2f4918fc0810037681867b
Dhruva-Kanthi <145169006+Dhruva-Kanthi@users.noreply.github.com>
2023-11-03 19:27:32 -0500

    made login work

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from flask import *
 
 app = Flask(__name__)
+username = 'Guest'
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', username=username)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    global username
 
+    error = ''
     if request.method == "POST":
-        username = request.form.get('username')
-        password = request.form.get('password')
-        return redirect(url_for('index'))
+        u = request.form.get('username')
+        p = request.form.get('password')
 
-    return render_template('login.html')
+        if u == 'myuser' and p == 'mypass':
+            username = 'myuser'
+            return redirect(url_for('index'))
+        
+        error = 'invalid login'
+
+    return render_template('login.html', error=error)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

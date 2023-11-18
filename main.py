8f3ad19075589780c981d64d287154e2d554db02
AsiyaRahman13 <jonparkerbrooks@gmail.com>
2023-11-17 19:30:59 -0600

    added the calculator

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ username = 'Guest'
 def index():
     return render_template('index.html', username=username)
 
+@app.route('/calculator', methods=["GET", "POST"])
+def calculator():
+
+    answer =  0
+    
+    if request.method == "POST":
+      x = float(request.form.get('x'))
+      y = float(request.form.get('y'))
+      op = request.form.get('operation')
+      if op == '+':
+         answer = x + y
+      elif op == "-":
+         answer = x - y
+        
+        
+        
+    return render_template('calc.html', answer=answer)
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
     global username

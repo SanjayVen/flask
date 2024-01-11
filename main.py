48ebee01e5a8761eedb120e679953c68ae88e20d
Gearminds Coder <studentrobotics.gearminds@gmail.com>
2024-01-10 18:58:28 -0600

    deleted some things

--- main.py
+++ main.py
@@ -26,39 +26,7 @@ def stock():
     session["stocks"].append(my_stock)
 
     return render_template("add_stock.html", stocks=session.get("stocks"))
-def calculator():
 
-    answer =  0
-    
-    if request.method == "POST":
-      x = float(request.form.get('x'))
-      y = float(request.form.get('y'))
-      op = request.form.get('operation')
-      if op == '+':
-         answer = x + y
-      elif op == "-":
-         answer = x - y
-        
-        
-        
-    return render_template('calc.html', answer=answer)
-
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    global username
-
-    error = ''
-    if request.method == "POST":
-        u = request.form.get('username')
-        p = request.form.get('password')
-
-        if u == 'myuser' and p == 'mypass':
-            username = 'myuser'
-            return redirect(url_for('index'))
-        
-        error = 'invalid login'
-
-    return render_template('login.html', error=error)
 
 
 if __name__ == "__main__":
--- quiz.py
+++ quiz.py
@@ -1,8 +1,1 @@
-def caught_speeding(speed):
-    result = 0
-    if speed > 60 and speed < 81:
-        result = 1
-    elif speed > 
-    81:
-        result = 2
-    return result 
+
